9c90ee746d8527fa3dc9175cb39f3603f2f02be7
kaushikchitipothu <kaushikchitipothu5@gmail.com>
2024-05-22 21:26:33 -0400

    updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../../Profile.jpeg";
+import profile from "../../Profile.jpg";
 import './About.css';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
@@ -13,7 +13,12 @@ function About(){
         <div className="container-main">
         <div className="horizontal-flex">
             <div className="box">
-            <img src={profile} alt="Profile" style={{borderRadius:'50%', width:'190px', height:'170px'}} />
+            <div>
+            <img src={profile} alt="Profile" style={{borderRadius:'50%', width:'200px', height:'200px'}} />
+            </div>
+            <div className="resume">
+                <a href={resume} download="KaushikChitipothu.pdf"><button className="btnResume"> <FontAwesomeIcon icon={faDownload}/> Resume </button></a>
+            </div>   
             </div>
             <div className="vertical-flex">
               <div className="container-inside">
@@ -21,15 +26,16 @@ function About(){
                 <br></br>
                 <h4>Hello,</h4>
                 <br></br>
-                <p>I am a Computer Science Graduate Student at the University of Central Florida with a work experience of 28 months including an internship, from which I secured a full-time role as an Associate Software Engineer. During these 2 years in the industry, I worked across various technologies like AngularJS, .NET, SQL, WIX Toolset, and Kubernetes to develop multiple web applications, desktop applications, and an installer.</p>
+                <p>I am a Computer Science graduate student at the University of Central Florida, set to graduate in May 2025.</p>
                 <br></br>
-                <p>My primary programming language is JAVA but I am comfortable with Python and C# as well. I had a great learning curve during my work experience, I am sure that I can handle myself if I come across any new technologies or tech stacks. As part of my responsibilities, I have developed various new features and improved a lot of existing features through code refactoring.</p>
+                <p>During my 6th semester of B.Tech, I interned as a Software Engineer at OpenText, where I primarily focused on deploying the product suite to the cloud using Kubernetes and Docker.</p>
                 <br></br>
-                <p style={{fontWeight:'bold'}}>I am currently looking for Summer 2024 internships. I am flexible with Full-Stack, Frontend, and Backend roles.</p>
+                <p>Following a successful 3-month internship, I transitioned into a full-time role as an Associate Software Engineer. In this role, I worked as a full stack developer with a primary focus on .NET and Angular. I developed crucial features such as implementing OAuth, developing RestAPIs for web applications, and designing user interfaces for multiple applications. Additionally, I developed an installer for our product suite from scratch.</p>
+                <br></br>
+                <p>After over 3 years of professional experience, I decided to further enhance my skills by pursuing a Master's degree in Computer Science. Over the past year, I have expanded my knowledge in various fields including computer vision, algorithms, malware analysis, and computer architecture. With this advanced education and my extensive experience, I believe I can be a valuable asset as a software developer.</p>
+                <br></br>
+                <p style={{fontWeight:'bold'}}>I am currently seeking developer roles and am flexible with Full-Stack, Frontend, and Backend positions.</p>
                 </div>
-                <div>
-                <a href={resume} download="KaushikChitipothu.pdf"><button className="btn-resume"> <FontAwesomeIcon icon={faDownload}/> Resume </button></a>
-                </div>   
               </div>
             </div>
         </div>
@@ -49,7 +55,7 @@ function About(){
            Master of Science in Computer Science
         </p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement className="vertical-timeline-element--work" date="Jun 2021 - Aug 2023"  iconStyle={{  color: '#fff', fontSize:'1.5rem' }}
+        <VerticalTimelineElement className="vertical-timeline-element--work" date="Jun 2020 - Aug 2023"  iconStyle={{  color: '#fff', fontSize:'1.5rem' }}
           icon={<img src={work} alt="myLogo" style={{ width: '100%', height: '100%' }} />} contentStyle={{ fontWeight:'bolder',backgroundColor: '#F5F5F5' }}>
         <h3 className="vertical-timeline-element-title">Opentext</h3>
         <h4 className="vertical-timeline-element-subtitle">Hyderabad, India</h4>
@@ -57,7 +63,7 @@ function About(){
           Associate Software Engineer
         </p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement className="vertical-timeline-element--work" date="Feb 2021 - Jun 2021"  iconStyle={{  color: '#fff', fontSize:'1.5rem' }}
+        <VerticalTimelineElement className="vertical-timeline-element--work" date="Feb 2020 - Jun 2020"  iconStyle={{  color: '#fff', fontSize:'1.5rem' }}
           icon={<img src={work} alt="myLogo" style={{ width: '100%', height: '100%' }}/>} contentStyle={{ backgroundColor: '#F5F5F5' }}>
         <h3 className="vertical-timeline-element-title">Opentext</h3>
         <h4 className="vertical-timeline-element-subtitle">Bangalore, India</h4>
